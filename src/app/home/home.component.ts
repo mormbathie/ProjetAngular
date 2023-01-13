88426986854b224c3bb3ce1e283d9d76a905cd45
mor mbathie <mormbathie98@gmail.com>
2023-01-13 14:58:14 +0000

    first commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  template: '<p>this inline</p>',
+
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+
+}
